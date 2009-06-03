4798997b8238dfe357cacb23e8be835d4f4d4b41
Jing Yu <jingyu@google.com>
2009-06-03 11:11:52 -0700

    Check in a new gcc-4.4.0 toolchain.
    gcc from google_vendor_src_branch @29689,
    binutils, gdb, gmp, mpfr from android-toolchain @149396
    This 32bit toolchain is built on gHardy.

---
+++ linux-x86/toolchain/arm-eabi-4.4.0/lib/gcc/arm-eabi/4.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../../gcc-4.4.0/configure --prefix=/usr/local --target=arm-eabi --host=i686-unknown-linux-gnu --build=i686-unknown-linux-gnu --enable-languages=c,c++ --with-gmp=/usr/local/google/home/projects/toolchain/android-toolchain/install_4.4_checkin/obj/temp-install --with-mpfr=/usr/local/google/home/projects/toolchain/android-toolchain/install_4.4_checkin/obj/temp-install --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --with-abi=aapcs --with-gcc-version=4.4.0 --with-binutils-version=2.19 --with-arch=armv5te --with-sysroot=/usr/local/google/home/projects/toolchain/android-toolchain/install_4.4_bin2.19/cupcake_rel_root --with-gmp-version=4.2.4 --with-mpfr-version=2.4.1 --with-gdb-version=6.6 --program-transform-name='s,^,arm-eabi-,'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs" }, { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
