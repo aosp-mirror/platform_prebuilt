406fda666bb7c8dd4de3294c3827a27c8fd4119c
Ying Wang <wangying@google.com>
2010-01-25 09:53:00 -0800

    Check in historical sdk versions.
    .jar and .aidl files are copied from released sdks.

--- Android.mk
+++ Android.mk
@@ -18,4 +18,5 @@ LOCAL_PATH := $(my-dir)
 include $(call first-makefiles-under, \
 		$(LOCAL_PATH)/common \
 		$(LOCAL_PATH)/$(TARGET_PREBUILT_TAG) \
-		$(LOCAL_PATH)/$(HOST_PREBUILT_TAG))
+		$(LOCAL_PATH)/$(HOST_PREBUILT_TAG) \
+		$(LOCAL_PATH)/sdk)
