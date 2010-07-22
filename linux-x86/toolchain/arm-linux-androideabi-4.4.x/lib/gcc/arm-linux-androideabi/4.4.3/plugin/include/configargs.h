a0f3510b73d5e7a1518f49866aad5f3106869d27
Jing Yu <jingyu@google.com>
2010-07-22 15:23:16 -0700

    Commit prebuilt arm-linux-androideabi toolchain. This androidebai target is already in upstream. Other than the new target, other toolchain sources are the same as the current arm-eabi-4.4.3 toolchain.
    Change-Id: I6fc41bf55d711ab03126d3183414afb3e11f69f2

---
+++ linux-x86/toolchain/arm-linux-androideabi-4.4.x/lib/gcc/arm-linux-androideabi/4.4.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/jingyu/projects/gcc/android-toolchainsrc/build/../gcc/gcc-4.4.3/configure --prefix=/usr/local --target=arm-linux-androideabi --host=x86_64-linux-gnu --build=x86_64-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-gmp=/home/jingyu/projects/gcc/toolchain_build/androidobj/temp-install --with-mpfr=/home/jingyu/projects/gcc/toolchain_build/androidobj/temp-install --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --disable-tls --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --with-gcc-version=4.4.3 --with-binutils-version=2.19 --with-gmp-version=4.2.4 --with-mpfr-version=2.4.1 --with-gdb-version=7.1.x --with-arch=armv5te --with-sysroot=/usr/local/google/home/android/froyo_rel_root --enable-gold=both/gold --program-transform-name='s&^&arm-linux-androideabi-&'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
