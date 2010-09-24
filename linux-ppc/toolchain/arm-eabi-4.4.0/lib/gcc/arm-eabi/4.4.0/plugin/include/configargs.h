1acc6ec12ca63f6358c5d2923111fa823be2cf24
Marcus Comstedt <marcus@mc.pp.se>
2010-09-24 22:23:05 +0200

    Toolchain for Linux/PPC.
    Change-Id: I22f66d2be076da158b8056fcd035fef0b2e19b21

---
+++ linux-ppc/toolchain/arm-eabi-4.4.0/lib/gcc/arm-eabi/4.4.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../build/../gcc/gcc-4.4.0/configure --prefix=/usr/local --target=arm-eabi --host=powerpc-unknown-linux-gnu --build=powerpc-unknown-linux-gnu --enable-languages=c,c++ --with-gmp=/home/marcus/hack/android/toolchain/objdir/temp-install --with-mpfr=/home/marcus/hack/android/toolchain/objdir/temp-install --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --disable-tls --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --with-abi=aapcs --with-gcc-version=4.4.0 --with-binutils-version=2.19 --with-gmp-version=4.2.4 --with-gdb-version=6.6 --with-mpfr-version=2.4.1 --program-transform-name='s&^&arm-eabi-&'";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs" }, { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
