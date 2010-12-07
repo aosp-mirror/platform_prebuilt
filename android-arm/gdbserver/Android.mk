f5033a8c79b8934b35f5efa1bc2a0b3231e7f24d
Doug Kwan <dougkwan@google.com>
2010-12-07 14:21:09 -0800

    Update gdbserver binary for gdb-7.1.x and fix remove the TLS hack. This is tested by running the gdb test suite, including many thread test on both a passion and a trygon. 98% of the test passed and only about 200 (2%) failed.
    Change-Id: Icb6e2a6d8ccf5e8af0b51931d039828abc31e447

--- android-arm/gdbserver/Android.mk
+++ android-arm/gdbserver/Android.mk
@@ -4,11 +4,7 @@ include $(CLEAR_VARS)
 # Explicitly mark gdbserver as "eng" so that it doesn't
 # get included in user or SDK builds. (GPL issues)
 #
-ifeq ($(ARCH_ARM_HAVE_TLS_REGISTER),true)
-LOCAL_SRC_FILES := gdbserver.tls
-else
 LOCAL_SRC_FILES := gdbserver
-endif
 LOCAL_MODULE := gdbserver
 LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_MODULE_TAGS := debug
