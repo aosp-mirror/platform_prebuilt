4e0d84a9e62cb6bb3e39320da3398d53329420ef
Xavier Ducrohet <xav@android.com>
2011-02-09 16:16:44 -0800

    Rename common-prebuilt.jar to tools-common-prebuilt.jar
    Change-Id: I92379eaac658352e6e914c253157677c68bd1b85

--- common/tools-common/Android.mk
+++ common/tools-common/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_JAVA_LIBRARIES := \
-	common-prebuilt$(COMMON_JAVA_PACKAGE_SUFFIX)
+	tools-common-prebuilt$(COMMON_JAVA_PACKAGE_SUFFIX)
 
 LOCAL_MODULE_TAGS := optional
 
