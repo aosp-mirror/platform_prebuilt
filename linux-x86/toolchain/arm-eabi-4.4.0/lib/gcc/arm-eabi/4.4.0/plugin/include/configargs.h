9d4d317ff5640b487849e9ef7151b68c9fe265ce
Jean-Baptiste Queru <jbq@google.com>
2011-12-02 10:08:55 -0800

    Delete unused toolchains
    Change-Id: Ia2ae8814a3796ff8497ea8ffe5e22afb4c0fa39c

--- linux-x86/toolchain/arm-eabi-4.4.0/lib/gcc/arm-eabi/4.4.0/plugin/include/configargs.h
+++
@@ -1,7 +0,0 @@
-/* Generated automatically. */
-static const char configuration_arguments[] = "/home/jingyu/projects/gcc/android-toolchainsrc/build/../gcc/gcc-4.4.0/configure --prefix=/usr/local --target=arm-eabi --host=x86_64-pc-linux-gnu --build=x86_64-pc-linux-gnu --enable-languages=c,c++ --with-gmp=/home/jingyu/projects/gcc/toolchain_build/obj/temp-install --with-mpfr=/home/jingyu/projects/gcc/toolchain_build/obj/temp-install --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-libstdc__-v3 --disable-sjlj-exceptions --disable-shared --disable-tls --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --with-abi=aapcs --with-gcc-version=4.4.0 --with-binutils-version=2.19 --with-gmp-version=4.2.4 --with-mpfr-version=2.4.1 --with-gdb-version=6.6 --with-arch=armv5te --with-multilib-list=mandroid --with-sysroot=/usr/local/google/home/android/cupcake_rel_root --enable-gold=both/gold --program-transform-name='s&^&arm-eabi-&'";
-static const char thread_model[] = "single";
-
-static const struct {
-  const char *name, *value;
-} configure_default_options[] = { { "abi", "aapcs" }, { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
