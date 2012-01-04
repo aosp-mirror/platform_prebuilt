171d318812a7c0f6a0d797d81e23c7298b5e2edb
Jing Yu <jingyu@google.com>
2012-01-03 16:02:39 -0800

    Check in gcc-4.6 and binutils-2.21 based toolchain
    Squashed commit of the following:
    
    commit fe8bff00cb552f8fa68cc7f275cd5ceea51904bd
    Author: Jing Yu <jingyu@google.com>
    Date:   Wed Dec 21 14:43:22 2011 -0800
    
        Update commits information of toolchain sources.
    
        Previous commits were not pushed to AOSP. The new
        commits are.
    
        Change-Id: I356693ba169902222d92c4635e0f7b33d02d9d80
    
    commit c3ddbf83c6642f5535cf1f2e3e024c3f1aad0142
    Author: Jing Yu <jingyu@google.com>
    Date:   Tue Nov 15 14:28:30 2011 -0800
    
        Patched binutils-2.21 for a gold bug.
    
        Change-Id: Id5b80c85510be8b3624212f9ffc4656d9972b762
    
    commit 7d751b2904b6a01645dc4e4d8ab71f21b7fd840f
    Author: Jing Yu <jingyu@google.com>
    Date:   Wed Nov 9 16:52:10 2011 -0800
    
        Commit gcc-4.6 and binutils-2.21 based toolchain.
    
        Refer to SOURCES for where to download toolchain sources in AOSP.
    
        Change-Id: I835b307a5b3bee8e5f507cd907e99fd5fe2baebc
    
    Change-Id: I0c104fa1317e35042f7ce0bdd8cc24477f6436c7

---
+++ darwin-x86/toolchain/arm-linux-androideabi-4.6/lib/gcc/arm-linux-androideabi/4.6.x-google/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/Volumes/project/toolchain/./src/build/../gcc/gcc-4.6/configure --prefix=/usr/local --target=arm-linux-androideabi --host=i686-apple-darwin --build=i686-apple-darwin --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-gmp=/Volumes/project/android/android-toolchain-build/v15release/obj/temp-install --with-mpfr=/Volumes/project/android/android-toolchain-build/v15release/obj/temp-install --with-mpc=/Volumes/project/android/android-toolchain-build/v15release/obj/temp-install --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-sjlj-exceptions --disable-shared --disable-tls --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --disable-hosted-libstdcxx --enable-cxx-flags='-fexceptions -frtti' --with-gcc-version=4.6 --with-binutils-version=2.21 --with-gmp-version=4.2.4 --with-mpfr-version=2.4.1 --with-gdb-version=7.1.x --with-arch=armv5te --with-sysroot=/Volumes/project/android/android-toolchain-build/v15release/install/sysroot --with-prefix=/Volumes/project/android/android-toolchain-build/v15release/install --with-gold-version=2.21 --enable-gold --program-transform-name='s&^&arm-linux-androideabi-&' --enable-gold=default";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
---
+++ linux-x86/toolchain/arm-linux-androideabi-4.6/lib/gcc/arm-linux-androideabi/4.6.x-google/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/usr/local/google/home/android/v15release/./src/build/../gcc/gcc-4.6/configure --prefix=/usr/local --target=arm-linux-androideabi --host=i686-linux-gnu --build=i686-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --with-gmp=/tmp/android/android-toolchain-build/v15/obj/temp-install --with-mpfr=/tmp/android/android-toolchain-build/v15/obj/temp-install --with-mpc=/tmp/android/android-toolchain-build/v15/obj/temp-install --disable-libssp --enable-threads --disable-nls --disable-libmudflap --disable-libgomp --disable-sjlj-exceptions --disable-shared --disable-tls --with-float=soft --with-fpu=vfp --with-arch=armv5te --enable-target-optspace --disable-hosted-libstdcxx --enable-cxx-flags='-fexceptions -frtti' --with-gcc-version=4.6 --with-binutils-version=2.21 --with-gmp-version=4.2.4 --with-mpfr-version=2.4.1 --with-gdb-version=7.1.x --with-arch=armv5te --with-sysroot=/tmp/android/android-toolchain-build/v15/install/sysroot --with-prefix=/tmp/android/android-toolchain-build/v15/install --with-gold-version=2.21 --enable-gold --program-transform-name='s&^&arm-linux-androideabi-&' --enable-gold=default";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv5te" }, { "float", "soft" }, { "fpu", "vfp" } };
