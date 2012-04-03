ef6ff875b9716096c8248cf3adeb40c5a3a9029d
Tor Norbye <tnorbye@google.com>
2012-04-03 10:31:20 -0700

    Build fix: Include old binaries too for branch usage
    Change-Id: Ie5e24bb18c86363d630b8a2c64c53b45c13a3b38

--- common/eclipse/Android.mk
+++ common/eclipse/Android.mk
@@ -10,7 +10,10 @@ LOCAL_PREBUILT_JAVA_LIBRARIES := \
 	org.eclipse.core.expressions_3.4.200.v20100505$(COMMON_JAVA_PACKAGE_SUFFIX) \
 	org.eclipse.core.runtime_3.6.0.v20100505$(COMMON_JAVA_PACKAGE_SUFFIX) \
 	org.eclipse.ui.workbench.texteditor_3.6.1.r361_v20100714-0800$(COMMON_JAVA_PACKAGE_SUFFIX) \
-	org.eclipse.ui.workbench_3.6.2.M20110210-1200$(COMMON_JAVA_PACKAGE_SUFFIX)
+	org.eclipse.ui.workbench_3.6.2.M20110210-1200$(COMMON_JAVA_PACKAGE_SUFFIX) \
+	org.eclipse.core.commands_3.4.0.I20080509-2000$(COMMON_JAVA_PACKAGE_SUFFIX) \
+	org.eclipse.equinox.common_3.4.0.v20080421-2006$(COMMON_JAVA_PACKAGE_SUFFIX) \
+	org.eclipse.jface_3.4.2.M20090107-0800$(COMMON_JAVA_PACKAGE_SUFFIX)
 
 LOCAL_MODULE_TAGS := optional
 
